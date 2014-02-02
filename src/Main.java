92f1d3314374a1c1d92302a1521c9db58291c420
Rob Clouth <rob.clouth@gmail.com>
2014-02-03 01:26:49 +1100

    Added restFB

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,25 @@
-import com.google.code.chatterbotapi.ChatterBot;
-import com.google.code.chatterbotapi.ChatterBotFactory;
-import com.google.code.chatterbotapi.ChatterBotSession;
-import com.google.code.chatterbotapi.ChatterBotType;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.Reader;
+import java.io.StringWriter;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import com.restfb.DefaultFacebookClient;
+import com.restfb.FacebookClient;
 
 
 public class Main {
+	static String userToken = "CAAC202USZCEsBAJAZBHfmQTiJRrUBaQmQUVnK4E7hHPzFOpZA9bxtIHQUUvOZAAMQZB5KFjllMwzSFFRoUIt4AXiuNQPnWzAdZAnxzdgYccDVAfHCs1wm0PMbYxVOaOGAsV7Mg2xgwN6UEQGSZA3Lb55dSZB8ZARj0FcwTsAFTjZAvesYlyuNPU2kA90GfaqBRHy8ZD";
+	
 	public static void main(String[] args) throws Exception {
-        ChatterBotFactory factory = new ChatterBotFactory();
-
-        ChatterBot bot1 = factory.create(ChatterBotType.JABBERWACKY);
-        ChatterBotSession bot1session = bot1.createSession();
-
-        ChatterBot bot2 = factory.create(ChatterBotType.JABBERWACKY);
-        ChatterBotSession bot2session = bot2.createSession();
-
-        String s = "How many bananas can you fit up your arse?";
-        while (true) {
-
-            System.out.println("bot1> " + s);
-
-            s = bot2session.think(s);
-            System.out.println("bot2> " + s);
-
-            s = bot1session.think(s);
-        }
-    }
+        FacebookClient facebookClient = new DefaultFacebookClient("CAACEdEose0cBALfkU7ShCXQALhXiNFLqnqtXgnZC5mu9fZCau3DqFWtNDtL7jrixV3v1g3Iw4jBXHxumC7z4D19pzSF0M7UaJwFSy4MGzXtQef41vCcuI0CeCgfgSojiOpcbQemFSLsNdawAI0Q9bj61ub9bpj2dWAhSPK2koZAA2sXfcnMqSDmUpqpZAZBoZD");
+    String response = Utils.post("https://graph.facebook.com/oauth/authorize?client_id=" +userToken + " & redirect_uri=http://www.facebook.com/connect/login_success.html& scope=publish_stream,create_event", new LinkedHashMap<String, String> () );
+    System.out.println(response);
+	}
+	
+	 
 }
